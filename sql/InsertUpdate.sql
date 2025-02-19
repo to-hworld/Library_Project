35b58ea7407ec31812d3d2673e3cb79055b58da4
JongIn <pubforwork@gmail.com>
2025-02-20 00:24:06 +0900

    write insertUpdate, selectDelete

---
+++ sql/InsertUpdate.sql
@@ -0,0 +1,28 @@
+--7 슬라이드 회원가입
+insert into MEMBER(ID,PASS,NAME,PHONE_NO,EMAIL,REG_LIB) values(:id,:pass,:name,:phone_no,:email,:library);
+
+--11 슬라이드 관심도서 담기
+insert into INTEREST_BOOK values(:id,:title,:isbn);
+
+--13 슬라이드
+--예약 버튼
+insert into BOOK_RESERVATION values(:id,:isbn,:title,:library);
+--상호대차
+insert into INTERLIBRARY values(:id,:isbn,:title,:holding_lib,:req_lib);
+
+--16 슬라이드 희망도서 신청
+insert into REQUESTED_BOOK(REQ_ID,ISBN,REQ_BOOK) values(:id,:isbn,:title);
+
+--20 슬라이드 글쓰기 / 작성 클릭 시 / 게시글 번호 SEQUENCE 사용 /파일 첨부는 따로 저장
+--비공개 체크
+insert into BOARD(BOARD_NO,BOARD_TITLE,WRITER_ID,OPEN) values(board_seq.NEXTVAL,:title,:id,:open);
+--비공개 미체크
+insert into BOARD(BOARD_NO,BOARD_TITLE,WRITER_ID) values(board_seq.NEXTVAL,:title,:id);
+--게시물 내용
+insert into BOARD_CONTENT values(board_content_seq.NEXTVAL,:content);
+--답변 등록
+update BOARD set REPLY='Y' where ID=:id and BOARD_NO=:num;
+
+--34 슬라이드
+update MEMBER set PASS=:PASS, PHONE_NO=:phone, EMAIL=:email where ID=:id;
+
---
+++ sql/selectDelete.sql
@@ -0,0 +1,41 @@
+--3 슬라이드 로그인
+select * from MEMBER where ID=:id and PASS=:pass;
+
+--9 슬라이드 자료검색
+--책 제목 선택
+select * from LIBRARY_BOOK where TITLE=:title and HOLDING_LIB in (':library1');
+select * from LIBRARY_BOOK where TITLE=:title and HOLDING_LIB in (':library1',':library2');
+select * from LIBRARY_BOOK where TITLE=:title and HOLDING_LIB in (':library1',':library2',':library3');
+--저자 선택
+select * from LIBRARY_BOOK where AUTHOR=:author and HOLDING_LIB in (':library1');
+select * from LIBRARY_BOOK where AUTHOR=:author and HOLDING_LIB in (':library1',':library2');
+select * from LIBRARY_BOOK where AUTHOR=:author and HOLDING_LIB in (':library1',':library2',':library3');
+
+--18 슬라이드 게시글 목록 / 기본 1페이지로 할 것
+select * from BOARD where rownum >= ((7*(1-:pagenum))+1) and rownum <= (7*:pagenum);
+
+--22 슬라이드
+select * from BOARD where WRITER_ID=:id;
+
+--24 슬라이드 조인 / 첨부 파일명은 따로 가져올 것
+select * from BOARD A inner join BOARD_CONTENT B ON A.BOARD_NO=B.CONTENT_NO where WRITER_ID=:id and BOARD_NO=:num;
+
+--26 슬라이드 조인
+select * from BOARD A inner join BOARD_CONTENT B ON A.BOARD_NO=B.CONTENT_NO where WRITER_ID=:id and BOARD_NO=:num;
+
+--30 슬라이드
+--대출현황
+select * from RENTAL_LIST where ID=:id;
+--예약현황
+select * from BOOK_RESERVATION where ID=:id;
+--관심도서
+select * from INTEREST_BOOK where MEM_ID=:id;
+--관심도서 삭제 버튼 클릭
+delete from INTEREST_BOOK where MEM_ID=:id and  TITLE=:title and ISBN=:isbn;
+--희망도서
+select * from REQUESTED_BOOK where REQ_ID=:id;
+
+--32 슬라이드
+select * from MEMBER where ID=:id and PASS=:pass;
+
+
