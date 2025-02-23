33271c10ffb91997f0ae4e9f0bf134115131adad
JongIn <pubforwork@gmail.com>
2025-02-23 16:05:52 +0900

    rewrite comment

--- sql/selectDelete.sql
+++ sql/selectDelete.sql
@@ -11,19 +11,19 @@ select * from LIBRARY_BOOK where AUTHOR=:author and HOLDING_LIB in (':library1')
 select * from LIBRARY_BOOK where AUTHOR=:author and HOLDING_LIB in (':library1',':library2');
 select * from LIBRARY_BOOK where AUTHOR=:author and HOLDING_LIB in (':library1',':library2',':library3');
 
---18 슬라이드 게시글 목록 / 기본 1페이지로 할 것
+--19 슬라이드 게시글 목록 / 기본 1페이지로 할 것
 select * from BOARD where rownum >= ((7*(1-:pagenum))+1) and rownum <= (7*:pagenum);
 
---22 슬라이드
+--23 슬라이드
 select * from BOARD where WRITER_ID=:id;
 
---24 슬라이드 조인 / 첨부 파일명은 따로 가져올 것
+--25 슬라이드 조인 / 첨부 파일명은 따로 가져올 것
 select * from BOARD A inner join BOARD_CONTENT B ON A.BOARD_NO=B.CONTENT_NO where WRITER_ID=:id and BOARD_NO=:num;
 
---26 슬라이드 조인
+--27 슬라이드 조인
 select * from BOARD A inner join BOARD_CONTENT B ON A.BOARD_NO=B.CONTENT_NO where WRITER_ID=:id and BOARD_NO=:num;
 
---30 슬라이드
+--31 슬라이드
 --대출현황
 select * from RENTAL_LIST where ID=:id;
 --예약현황
@@ -35,7 +35,7 @@ delete from INTEREST_BOOK where MEM_ID=:id and  TITLE=:title and ISBN=:isbn;
 --희망도서
 select * from REQUESTED_BOOK where REQ_ID=:id;
 
---32 슬라이드
+--33 슬라이드
 select * from MEMBER where ID=:id and PASS=:pass;
 
 
